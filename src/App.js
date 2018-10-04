e308ac532ca294658238bd73ca620700944e6cd6
Tony Duco <nosduco@gmail.com>
2018-10-04 19:16:07 -0400

    Function React app with Bulma framework. NavBar implemented

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
+import 'react-bulma-components/src/index.sass';
+import Navbar from 'react-bulma-components/lib/components/navbar';
+import hacklogo from './img/hackohiologo.png';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <Navbar
+            color="dark"
+        >
+            <Navbar.Brand>
+                <Navbar.Item renderAs="a" href="#">
+                    <img
+                        src={hacklogo}
+                        alt="HackOhio Logo"
+                    />
+                </Navbar.Item>
+            </Navbar.Brand>
+            <Navbar.Menu active={Navbar.Menu.open}>
+                <Navbar.Container>
+                    <Navbar.Item href="#">Team Separator</Navbar.Item>
+                </Navbar.Container>
+                <Navbar.Container position="end">
+                    <Navbar.Item>Alpha Testing</Navbar.Item>
+                </Navbar.Container>
+            </Navbar.Menu>
+        </Navbar>
       </div>
     );
   }
